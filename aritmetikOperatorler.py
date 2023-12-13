954a9b212fd8ca57089945b84c316768ad36506f
gunalhakan <64796323+gunalhakan@users.noreply.github.com>
2023-12-13 22:59:52 +0300

    upload ile yeni dosyalar eklendi.
    Çalışma ortamı tek düzene indirildi

---
+++ aritmetikOperatorler.py
@@ -0,0 +1,20 @@
+'''
+= Atama operatörü
++ Toplama operatörü
+- Çıkarma operatörü
+* Çarpma operatörü
+/ Bölme operatörü
+// Tam Bölme operatörü
+% Kalan operatörü
+** Üst Alma operatörü
+'''
+sayi1 = 4 # = atama opearatörüdür, soldaki değer sağdaki değişkene aktarıldı
+sayi2 = 3
+sonuc = sayi1 + sayi2 # + ile toplama işlemi yapıldı, değer = ile sonuc değişkenine aktarıldı
+print(sonuc)
+print(sayi1 - sayi2)
+print(sayi1 * sayi2)
+print(sayi1 / sayi2) # sonucu float cinsinden hesaplar
+print(sayi1 // sayi2) # ondalıklı kısmı atıp sonucu integer olarak hesaplar
+print(sayi1 % sayi2) # kalan değeri hesaplar
+print(sayi1 ** sayi2) # üst değerini hesaplar
---
+++ degiskenler.py
@@ -0,0 +1,8 @@
+isim = "Ali" #string
+yas = 19 #integer
+onay = True # boolean
+boy = 1.63 # double
+print("İsim=", isim)
+print("Yaş=", yas)
+print("Boy=",boy)
+print("Onay=",onay)
